38aeec25725f55e67a37d2a38e1ac95ad25473a0
ninhadmit147 <59793130+ninhadmit147@users.noreply.github.com>
2022-10-16 01:47:23 +0700

    Update validation form add product

--- src/app/components/add-pro/add-pro.component.ts
+++ src/app/components/add-pro/add-pro.component.ts
@@ -4,7 +4,7 @@ import { CategoryService } from './../../services/category.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IProduct } from 'src/app/models/product';
 
 @Component({
@@ -23,31 +23,44 @@ export class AddProComponent implements OnInit {
     categoryId: '',
     img: '',
   };
+  productForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    price: new FormControl('', [Validators.required]),
+    category: new FormControl('', [Validators.required]),
+    desc: new FormControl('', [Validators.required])
+  })
   constructor(
     private ActivatedRoute: ActivatedRoute,
     private productService: ProductService,
     private cateService: CategoryService,
     private Router: Router
-  ) {}
+  ) { }
   ngOnInit(): void {
     this.getCate();
+
   }
-  
-  getCate(){
-    this.cateService.getAllCate().subscribe(data=>{
+
+  getCate() {
+    this.cateService.getAllCate().subscribe(data => {
       this.categories = data
     })
   }
 
   onHandleAdd() {
-    this.productService.addProduct(this.product).subscribe(res => {
-      console.log(res);
-      alert("Đăng ký thành công");
-      this.Router.navigateByUrl("admin")
-    },
-    err => {
-      alert("Sai cái j rồi!!!!")
-    })
+    if (this.productForm.value.name == "" || this.productForm.value.price == "" || this.productForm.value.category == "" || this.productForm.value.desc == "") {
+      console.log('check form');
+
+    } else {
+      this.productService.addProduct(this.product).subscribe(res => {
+        console.log(res);
+        alert("Đăng ký thành công");
+        this.Router.navigateByUrl("admin")
+      },
+        err => {
+          alert("Sai cái j rồi!!!!")
+        })
+    }
   }
+
 
 }
