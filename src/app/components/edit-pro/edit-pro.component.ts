7e602887e7ccc17585412552eca24c235ea6c92c
Duongld0511 <duongld.0511@gmail.com>
2022-10-15 11:49:46 +0700

    Fix admin dash board

---
+++ src/app/components/edit-pro/edit-pro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-pro',
+  templateUrl: './edit-pro.component.html',
+  styleUrls: ['./edit-pro.component.css']
+})
+export class EditProComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
